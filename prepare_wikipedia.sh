a8e81f202e1cbbd30e68d91f1f99d39a6ac93380
Matthias Bachfischer <bachfischer.matthias@googlemail.com>
2018-06-07 13:53:55 +0300

    fixed data extaraction script from Wikipedia

--- prepare_wikipedia.sh
+++ prepare_wikipedia.sh
@@ -7,4 +7,5 @@ python setup.py install
 curl -O https://dumps.wikimedia.org/dewiki/latest/dewiki-latest-pages-articles.xml.bz2
 bzip2 -dk dewiki-latest-pages-articles.xml.bz2
 
-python3 WikiExtractor.py -o data/ dewiki-latest-pages-articles.xml -s --json
+python3 WikiExtractor.py dewiki-latest-pages-articles.xml -o extract -b 10M --ignored_tags abbr,b,big --discard_elements gallery,timeline,noinclude
+
